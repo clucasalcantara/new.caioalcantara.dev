5dd54c90a5369f7c6fde34f44bae44aa6e75325f
Caio Alcantara <alcantaracaiolucas@gmail.com>
2022-02-25 11:29:31 -0300

    style: adjust viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700;900&display=swap"
             rel="stylesheet"
           />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main />
